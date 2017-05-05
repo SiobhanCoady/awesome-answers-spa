af952e2d74b84984742794a002ea73b67c8dff29
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-05 14:59:09 -0700

    Add question details and basic css

--- index.js
+++ index.js
@@ -19,11 +19,23 @@ function renderQuestions(questions) {
   return questions.map(function(question) {
     return `
       <div class="question-summary">
-        ${question.title}
+        <a
+          data-id=${question.id}
+          href
+          class="question-link">
+            ${question.title}
+        </a>
       </div>
-      <hr>
     `
   }).join('');
+}
+
+function renderQuestion(question) {
+  return `
+    <button class="back">Back</button>
+    <h1>${question.title}</h1>
+    <p>${question.body}</p>
+  `
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -32,8 +44,36 @@ document.addEventListener('DOMContentLoaded', function() {
   // DOMContentLoaded event handler will run once every HTML tag has been
   // rendered by the browser.
   const questionsList = document.querySelector('#questions-list');
+  const questionDetails = document.querySelector('#question-details');
 
   getQuestions()
     .then(renderQuestions)
     .then(function(html) { questionsList.innerHTML = html })
-})
+
+  questionsList.addEventListener('click', function(event) {
+    const { target } = event;
+
+    if (target.matches('.question-link')) {
+      event.preventDefault();
+      const questionId = target.getAttribute('data-id');
+
+
+      getQuestion(questionId)
+        .then(function(question) {
+          questionDetails.innerHTML = renderQuestion(question);
+          questionDetails.classList.remove('hidden');
+          questionsList.classList.add('hidden');
+        });
+    }
+
+  });
+
+  questionDetails.addEventListener('click', function(event) {
+    const { target } = event;
+
+    if (target.matches('button.back')) {
+      questionDetails.classList.add('hidden');
+      questionsList.classList.remove('hidden');
+    }
+  });
+});
